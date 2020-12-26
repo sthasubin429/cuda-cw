5262ed7b6ed8498441bd45780d5c753abcc3a655
subin <subinshrestha429@gmail.com>
2020-12-26 10:56:14 +0545

    Gaussian Blur added

--- 6cs005_PortfolioS1_20_21_2039281_Subin_Shrestha/2039281_Task3_B.cu
+++ 6cs005_PortfolioS1_20_21_2039281_Subin_Shrestha/2039281_Task3_B.cu
@@ -1,314 +1,188 @@
-#include <stdio.h>
+//nvcc 2039281_Task3_B.cu lodepng.cpp -o task3_B 
+//task3_B hck.png
+#include "lodepng.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "lodepng.h"
-
-__global__ void gaussian_red(unsigned char* img_in, unsigned char* img_out){
-    int a = threadIdx.x; //height
-    int b = blockIdx.x; //width
-    int c = blockDim.x; //dimentions
-    int count = blockDim.x * threadIdx.x + blockIdx.x;
-
-    int weight_mat[9] = {1,2,1,2,4,2,1,2,1};
-    int sum;
-    
-    signed int num[9][2];
-    num[0][0] = a-1;
-    num[0][1] = b-1;
-    num[1][0] = a-1;
-    num[1][1] = b;
-    num[2][0] = a-1;
-    num[2][1] = b+1;
-    num[3][0] = a;
-    num[3][1] = b-1;
-    num[4][0] = a;
-    num[4][1] = b;
-    num[5][0] = a;
-    num[5][1] = b+1;
-    num[6][0] = a+1;
-    num[6][1] = b-1;
-    num[7][0] = a+1;
-    num[7][1] = b;
-    num[8][0] = a+1;
-    num[8][1] = b+1;
-
-    int mat[9];
-    for(int i =0;i<9;i++){
-        if(num[i][0] < 0 || num[i][1] < 0 || num[i][0] > 3|| num[i][1]>3){
-            mat[i] = -1;
-        }
-        else{
-            mat[i] = num[i][0] + c*num[i][1];
-        }
-    }
-    sum=0;
-    for(int i=0;i<9;i++){
-        if(mat[i] == -1){
-            sum = sum + (0*weight_mat[i]);
-        }
-        else{
-            
-            sum = sum + (img_in[mat[i]]*weight_mat[i]);
-        }
-    }
-    sum = sum/16;
-
-    img_out[4*count] = sum;
-
-    //printf("%d ", sum);
-    //printf("a: %d, b: %d, c: %d, count: %d \n", a,b,c,count);
+#include <cuda_runtime_api.h>
+__device__ unsigned int width;
+__device__ unsigned int height;
+__device__ unsigned char getRed(unsigned char *image, unsigned int row, unsigned int col){
+  unsigned int i = (row * width * 4) + (col * 4);
+  return image[i];
 }
-__global__ void gaussian_green(unsigned char* img_in, unsigned char* img_out){
-    int a = threadIdx.x; //height
-    int b = blockIdx.x; //width
-    int c = blockDim.x; //dimentions
-    int count = blockDim.x * threadIdx.x + blockIdx.x;
 
-    int weight_mat[9] = {1,2,1,2,4,2,1,2,1};
-    int sum;
-    
-    signed int num[9][2];
-    num[0][0] = a-1;
-    num[0][1] = b-1;
-    num[1][0] = a-1;
-    num[1][1] = b;
-    num[2][0] = a-1;
-    num[2][1] = b+1;
-    num[3][0] = a;
-    num[3][1] = b-1;
-    num[4][0] = a;
-    num[4][1] = b;
-    num[5][0] = a;
-    num[5][1] = b+1;
-    num[6][0] = a+1;
-    num[6][1] = b-1;
-    num[7][0] = a+1;
-    num[7][1] = b;
-    num[8][0] = a+1;
-    num[8][1] = b+1;
-
-    int mat[9];
-    for(int i =0;i<9;i++){
-        if(num[i][0] < 0 || num[i][1] < 0 || num[i][0] > 3|| num[i][1]>3){
-            mat[i] = -1;
-        }
-        else{
-            mat[i] = num[i][0] + c*num[i][1];
-        }
-    }
-    sum=0;
-    for(int i=0;i<9;i++){
-        if(mat[i] == -1){
-            sum = sum + (0*weight_mat[i]);
-        }
-        else{
-            
-            sum = sum + (img_in[mat[i]]*weight_mat[i]);
-        }
-    }
-    sum = sum/16;
-
-    img_out[(4*count)+1] = sum;
-
-    //printf("%d ", sum);
-    //printf("a: %d, b: %d, c: %d, count: %d \n", a,b,c,count);
+__device__ unsigned char getGreen(unsigned char *image, unsigned int row, unsigned int col){
+  unsigned int i = (row * width * 4) + (col * 4) +1;
+  return image[i];
 }
-__global__ void gaussian_blue(unsigned char* img_in, unsigned char* img_out){
-    int a = threadIdx.x; //height
-    int b = blockIdx.x; //width
-    int c = blockDim.x; //dimentions
-    int count = blockDim.x * threadIdx.x + blockIdx.x;
 
-    int weight_mat[9] = {1,2,1,2,4,2,1,2,1};
-    int sum;
-    
-    signed int num[9][2];
-    num[0][0] = a-1;
-    num[0][1] = b-1;
-    num[1][0] = a-1;
-    num[1][1] = b;
-    num[2][0] = a-1;
-    num[2][1] = b+1;
-    num[3][0] = a;
-    num[3][1] = b-1;
-    num[4][0] = a;
-    num[4][1] = b;
-    num[5][0] = a;
-    num[5][1] = b+1;
-    num[6][0] = a+1;
-    num[6][1] = b-1;
-    num[7][0] = a+1;
-    num[7][1] = b;
-    num[8][0] = a+1;
-    num[8][1] = b+1;
-
-    int mat[9];
-    for(int i =0;i<9;i++){
-        if(num[i][0] < 0 || num[i][1] < 0 || num[i][0] > 3|| num[i][1]>3){
-            mat[i] = -1;
-        }
-        else{
-            mat[i] = num[i][0] + c*num[i][1];
-        }
-    }
-    sum=0;
-    for(int i=0;i<9;i++){
-        if(mat[i] == -1){
-            sum = sum + (0*weight_mat[i]);
-        }
-        else{
-            
-            sum = sum + (img_in[mat[i]]*weight_mat[i]);
-        }
-    }
-    sum = sum/16;
-
-    img_out[(4*count)+2] = sum;
-
-    //printf("%d ", sum);
-    //printf("a: %d, b: %d, c: %d, count: %d \n", a,b,c,count);
+__device__ unsigned char getBlue(unsigned char *image, unsigned int row, unsigned int col){
+  unsigned int i = (row * width * 4) + (col * 4) +2;
+  return image[i];
 }
-__global__ void gaussian_transparent(unsigned char* img_in, unsigned char* img_out){
-    int a = threadIdx.x; //height
-    int b = blockIdx.x; //width
-    int c = blockDim.x; //dimentions
-    int count = blockDim.x * threadIdx.x + blockIdx.x;
 
-    int weight_mat[9] = {1,2,1,2,4,2,1,2,1};
-    int sum;
-    
-    signed int num[9][2];
-    num[0][0] = a-1;
-    num[0][1] = b-1;
-    num[1][0] = a-1;
-    num[1][1] = b;
-    num[2][0] = a-1;
-    num[2][1] = b+1;
-    num[3][0] = a;
-    num[3][1] = b-1;
-    num[4][0] = a;
-    num[4][1] = b;
-    num[5][0] = a;
-    num[5][1] = b+1;
-    num[6][0] = a+1;
-    num[6][1] = b-1;
-    num[7][0] = a+1;
-    num[7][1] = b;
-    num[8][0] = a+1;
-    num[8][1] = b+1;
-
-    int mat[9];
-    for(int i =0;i<9;i++){
-        if(num[i][0] < 0 || num[i][1] < 0 || num[i][0] > 3|| num[i][1]>3){
-            mat[i] = -1;
-        }
-        else{
-            mat[i] = num[i][0] + c*num[i][1];
-        }
-    }
-    sum=0;
-    for(int i=0;i<9;i++){
-        if(mat[i] == -1){
-            sum = sum + (0*weight_mat[i]);
-        }
-        else{
-            
-            sum = sum + (img_in[mat[i]]*weight_mat[i]);
-        }
-    }
-    sum = sum/16;
-
-    img_out[(4*count)+3] = sum;
-
-    //printf("%d ", sum);
-    //printf("a: %d, b: %d, c: %d, count: %d \n", a,b,c,count);
+__device__ unsigned char getAlpha(unsigned char *image, unsigned int row, unsigned int col){
+  unsigned int i = (row * width * 4) + (col * 4) +3;
+  return image[i];
 }
+
+__device__ void setRed(unsigned char *image, unsigned int row, unsigned int col, unsigned char red){
+  unsigned int i = (row * width * 4) + (col * 4);
+  image[i] = red;
+}
+
+__device__ void setGreen(unsigned char *image, unsigned int row, unsigned int col, unsigned char green){
+  unsigned int i = (row * width * 4) + (col * 4) +1;
+  image[i] = green;
+}
+
+__device__ void setBlue(unsigned char *image, unsigned int row, unsigned int col, unsigned char blue){
+  unsigned int i = (row * width * 4) + (col * 4) +2;
+  image[i] = blue;
+}
+
+__device__ void setAlpha(unsigned char *image, unsigned int row, unsigned int col, unsigned char alpha){
+  unsigned int i = (row * width * 4) + (col * 4) +3;
+  image[i] = alpha;
+}
+
+__global__ void cudaBlur(unsigned char* img_in, unsigned char* newImage){
+    if(blockIdx.x == 0 || threadIdx.x == 0 ){
+
+    }
+    else{
+        float filter[3][3] = {
+  { 1.0/16, 2.0/16, 1.0/16 },
+  { 2.0/16, 4.0/16, 2.0/16 },
+  { 1.0/16, 2.0/16, 1.0/16 }};
+
+    unsigned redTL,redTC, redTR;
+    unsigned redL, redC, redR;
+    unsigned redBL,redBC, redBR;
+    unsigned newRed;
+
+    unsigned greenTL,greenTC, greenTR;
+    unsigned greenL, greenC, greenR;
+    unsigned greenBL,greenBC, greenBR;
+    unsigned newGreen;
+
+    unsigned blueTL,blueTC, blueTR;
+    unsigned blueL, blueC, blueR;
+    unsigned blueBL,blueBC, blueBR;
+    unsigned newBlue;
+
+    int row = threadIdx.x;
+    int col = blockIdx.x;
+
+      setGreen(newImage, row, col, getGreen(img_in, row, col));
+      setBlue(newImage, row, col, getBlue(img_in, row, col));
+      setAlpha(newImage, row, col, 255);		
+
+      redTL = getRed(img_in, row-1, col-1);
+      redTC = getRed(img_in, row-1, col);
+      redTR = getRed(img_in, row-1, col+1);
+
+      redL = getRed(img_in, row, col-1);
+      redC = getRed(img_in, row, col);
+      redR = getRed(img_in, row, col+1);
+
+      redBL = getRed(img_in, row+1, col-1);
+      redBC = getRed(img_in, row+1, col);
+      redBR = getRed(img_in, row+1, col+1);
+
+      newRed = redTL*filter[0][0] + redTC*filter[0][1] + redTR*filter[0][2]
+             + redL*filter[1][0]  + redC*filter[1][1]  + redR*filter[1][2]
+             + redBL*filter[2][0] + redBC*filter[2][1] + redBR*filter[2][2];
+ 
+      setRed(newImage, row, col, newRed);
+
+      greenTL = getGreen(img_in, row-1, col-1);
+      greenTC = getGreen(img_in, row-1, col);
+      greenTR = getGreen(img_in, row-1, col+1);
+
+      greenL = getGreen(img_in, row, col-1);
+      greenC = getGreen(img_in, row, col);
+      greenR = getGreen(img_in, row, col+1);
+
+      greenBL = getGreen(img_in, row+1, col-1);
+      greenBC = getGreen(img_in, row+1, col);
+      greenBR = getGreen(img_in, row+1, col+1);
+
+      newGreen = greenTL*filter[0][0] + greenTC*filter[0][1] + greenTR*filter[0][2]
+             + greenL*filter[1][0]  + greenC*filter[1][1]  + greenR*filter[1][2]
+             + greenBL*filter[2][0] + greenBC*filter[2][1] + greenBR*filter[2][2];
+ 
+      setGreen(newImage, row, col, newGreen);
+
+      blueTL = getBlue(img_in, row-1, col-1);
+      blueTC = getBlue(img_in, row-1, col);
+      blueTR = getBlue(img_in, row-1, col+1);
+
+      blueL = getBlue(img_in, row, col-1);
+      blueC = getBlue(img_in, row, col);
+      blueR = getBlue(img_in, row, col+1);
+
+      blueBL = getBlue(img_in, row+1, col-1);
+      blueBC = getBlue(img_in, row+1, col);
+      blueBR = getBlue(img_in, row+1, col+1);
+
+      newBlue = blueTL*filter[0][0] + blueTC*filter[0][1] + blueTR*filter[0][2]
+             + blueL*filter[1][0]  + blueC*filter[1][1]  + blueR*filter[1][2]
+             + blueBL*filter[2][0] + blueBC*filter[2][1] + blueBR*filter[2][2];
+ 
+      setBlue(newImage, row, col, newBlue);        
+    }
+    
+}
+
+__global__ void setDimentions(unsigned int givenWidth, unsigned int givenHeight){
+    width = givenWidth;
+    height = givenHeight;
+    
+}
+
 int main(int argc, char **argv){
+    unsigned char *image;
+    unsigned int w;
+    unsigned int h;
+    const char* filename = argv[1];
+    const char* img_output = "generated.png";
+    unsigned char* output_raw;
+    cudaError_t error;
+    
+    lodepng_decode32_file(&image, &w, &h, filename);
+    
+    printf("width = %d height = %d\n", w, h);
 
-  cudaError_t cudaError;
-	unsigned int error;
-	unsigned int encError;
-	unsigned char* image;
-	unsigned int width;
-	unsigned int height;
-	const char* filename = "4x4.png";
-  const char* newFileName = "generated.png";
+    setDimentions<<<1,1>>>(w, h);
 
-  
-	error = lodepng_decode32_file(&image, &width, &height, filename);
+    unsigned char * d_image;
+    const int ARRAY_BYTES = h*w*4 * sizeof(unsigned char);
 
-  unsigned char r[height*width];
-	unsigned char g[height*width];
-	unsigned char b[height*width];
-	unsigned char t[height*width]; //transparency
-	
-  if(error){
-		printf("error %u: %s\n", error, lodepng_error_text(error));
-  }
+    output_raw = (unsigned char *)malloc(ARRAY_BYTES);
 
-   printf("width = %d height = %d\n", width, height);
-    for(int i = 0; i<height*width*4; i=i+4){
-      r[i/4] = image[i];
-      g[i/4] = image[1+i];
-      b[i/4] = image[2+i];
-      t[i/4] = image[3+i];
+    cudaMalloc((void**) &d_image, ARRAY_BYTES);
+    cudaMemcpy(d_image, image, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+    unsigned char * d_out;
+    cudaMalloc((void**) &d_out, ARRAY_BYTES);
+
+    cudaBlur<<<w-1, h-1>>>(d_image, d_out);
+    //Declaration of cuda thread synchronize
+    error = cudaGetLastError();
+    if(error){
+      fprintf(stderr, "Kernel launch returned %d %s\n", error,
+        cudaGetErrorString(error));
+      exit(1);
     }
+    cudaDeviceSynchronize();
 
-  printf("\n");
-  const int ARRAY_BYTES = height*width * sizeof(unsigned char);
-  unsigned char h_output[height*width*4];
+    cudaMemcpy(output_raw, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
+    
+    lodepng_encode32_file(img_output, output_raw, w, h);
 
-  // declare GPU memory pointers for rgbt arrays
-  unsigned char * d_red;
-  unsigned char * d_green;
-  unsigned char * d_blue;
-  unsigned char * d_transparent;
+    free(image);
 
-  unsigned char * d_output;
-
-  // allocate GPU memory For rgbt arrays
-  cudaMalloc((void**) &d_red, ARRAY_BYTES);
-  cudaMalloc((void**) &d_green, ARRAY_BYTES);
-  cudaMalloc((void**) &d_blue, ARRAY_BYTES);
-  cudaMalloc((void**) &d_transparent, ARRAY_BYTES);
-
-  cudaMalloc((void**) &d_output, ARRAY_BYTES);
-
-  //Memory copy
-  cudaMemcpy(d_red, r, ARRAY_BYTES, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_green, g, ARRAY_BYTES, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_blue, b, ARRAY_BYTES, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_transparent, t, ARRAY_BYTES, cudaMemcpyHostToDevice);
-
-  //<<<blockid.x,threadid.x >>>
-  gaussian_red<<<width, height>>>(d_red, d_output);
-  gaussian_red<<<width, height>>>(d_green, d_output);
-  gaussian_red<<<width, height>>>(d_blue, d_output);
-  gaussian_red<<<width, height>>>(d_transparent, d_output);
-
-  cudaDeviceSynchronize();
-  // copy back the result array to the CPU
-  cudaMemcpy(h_output, d_output, ARRAY_BYTES, cudaMemcpyDeviceToHost);
-  
-  for(int i = 0; i<height*width*4; i=i+4){
-      printf("%d %d %d %d\n", h_output[i], h_output[i+1], h_output[i+2], h_output[i+3]);
-  }
-  cudaError = cudaGetLastError();
-  
-  if(cudaError){
-    fprintf(stderr, "Kernel launch returned %d %s\n", 
-    cudaError, cudaGetErrorString(cudaError));
-    return 1;
-  } else {
-    fprintf(stderr, "Kernel launch successful.\n");
-  }
-  encError = lodepng_encode32_file(newFileName, h_output, width, height);
-	if(encError){
-		printf("error %u: %s\n", error, lodepng_error_text(encError));
-	}
-       
-      
-	return 0;
+    return 0;
 }
